0836d83da475e50d2e77e2b140f7d22745ce7937
MacyL <wumeishin@gmail.com>
2020-01-20 15:00:48 +0100

    upload CLDF skeleton

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='cldfbench_clarkkimmun',
+    py_modules=['cldfbench_clarkkimmun'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'cldfbench.dataset': [
+            'clarkkimmun=cldfbench_clarkkimmun:Dataset',
+        ]
+    },
+    install_requires=[
+        'cldfbench',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
