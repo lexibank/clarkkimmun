8b3c318e77e60b9bf0e33007d531e0dc100b9fe6
MacyL <wumeishin@gmail.com>
2020-01-27 17:11:35 +0100

    update scripts

--- cldfbench_clarkkimmun.py
+++ cldfbench_clarkkimmun.py
@@ -2,6 +2,7 @@ from collections import OrderedDict
 import pathlib
 
 import attr
+import lingpy
 from clldutils.misc import slug
 from pylexibank import Concept, Language
 from pylexibank.dataset import Dataset as MyDataset
@@ -11,21 +12,29 @@ from pylexibank.util import progressbar
 @attr.s
 class CustomConcept(Concept):
     Gloss_in_Source = attr.ib(default=None)
+    Doculect = attr.ib(default=None)
 
 @attr.s
 class CustomLanguage(Language):
     Latitude = attr.ib(default=None)
     Longitude = attr.ib(default=None)
     Family = attr.ib(default="Hmong-Mien")
-    DataSource = attr.ib(default=None)
+    Country = attr.ib(default=None)
     Name_in_Source = attr.ib(default=None)
-    Location = attr.ib(default=None)
+    ISO = attr.ib(default='mji')
 
-
-
-class Dataset(BaseDataset):
+class Dataset(MyDataset):
     dir = pathlib.Path(__file__).parent
     id = "clarkkimmun"
+    language_class = CustomLanguage
+    concept_class = CustomConcept
+    form_spec = FormSpec(
+        missing_data=['', '-------'],
+        separators=";/,",
+        brackets={'(': ')', '[': ']'},
+        strip_inside_brackets=True,
+        first_form_only=True
+    )
 
     def cldf_specs(self):  # A dataset must declare all CLDF sets it creates.
         return super().cldf_specs()
@@ -44,3 +53,69 @@ class Dataset(BaseDataset):
 
         >>> args.writer.objects['LanguageTable'].append(...)
         """
+        # add source
+        args.writer.add_sources()
+        # read in raw data, Laos and Vietnam
+        Laos = self.raw_dir.read_csv("KimMun_Laos.tsv",
+                dicts=True,
+                delimiter="\t")
+        Vietnam = self.raw_dir.read_csv("KimMun_Vietnam.tsv",
+                dicts=True,
+                delimiter="\t")
+        #combine data
+        Data={0:['ID',
+                'concept',
+                'doculect',
+                'ipa']}
+        i = 1
+        for each in Laos:
+            Data[i] = ['Laos_'+each['ID'],
+                        each['English'],
+                        'Luang Nam Tha, Laos',
+                        each['Luang Nam Tha, Laos']]
+            i = i + 1
+        v = len(Data)
+        for each in Vietnam:
+            Data[v] = ['Vietnam_'+each['ID'],
+                       each['English'],
+                       'Lao Cai, Vietnam',
+                       each['Lao Cai, Vietnam']]
+            v = v +1
+        #print(Data)
+        wl = lingpy.Wordlist(Data)
+        for idx, val in wl.iter_rows('ipa'):
+            print(idx, wl[idx])
+        languages = {}
+        for lang in self.languages:
+            print(lang)
+            args.writer.add_language(
+                 ID = lang['ID'],
+                 Name = lang['Name'],
+                 Latitude = lang['Latitude'],
+                 Longitude = lang['Longitude']
+             )
+            languages[lang['Name']]={'ID': lang['ID']}
+        print(languages)
+        # make concept dictionary
+        concepts = {}
+        for concept in self.concepts:
+            idx = concept['ID']+'_'+slug(concept['GLOSS'])
+            args.writer.add_concept(
+                ID=idx,
+                Name=concept['GLOSS'])
+            concepts[concept['GLOSS']]=idx
+        # create forms
+        for cogid_, idx in progressbar(
+            enumerate(wl), desc = 'cldfify the data'):
+            cogid = cogid_ + 1
+            if wl[idx, "value"]:
+                lex=args.writer.add_forms_from_value(
+                    Language_ID=languages[wl[idx, "doculect"]],
+                    Parameter_ID=concepts[wl[idx, "concept"]],
+                    Value=wl[idx, "ipa"]
+                )
+            # add cognate
+                args.writer.add_cognate(
+                    lexeme=lex,
+                    Cognateset_ID = cogid
+                )
